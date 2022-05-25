2178eae9205e62e3242ca4f462eace695e098450
Jean$ <jeancarlossoares.s@gmail.com>
2022-05-25 15:34:16 -0300

    Starting project & Db Connection

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');   
+
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log(`O Express está rodando  na porta ${PORT}`);
+});
+
+// db connection
+db
+.authenticate()
+.then(() => {
+    console.log("Conectou ao banco de dados com sucesso");
+})
+.catch(err => {
+    console.log("Ocorreu um erro ao conectar com o banco de dados", err);
+});
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Aplicação funcionando!");
+});
