6a68bf86bd6cd4fec5468e05dd1019bf1eb2e4e3
Jean$ <jeancarlossoares.s@gmail.com>
2022-05-27 15:30:09 -0300

    Inserting data in DB via Postman

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,20 +3,21 @@ const res = require("express/lib/response");
 const router = express.Router();
 const Job = require("../models/Job");
 
-router.get('/test', (req, res) => {
+
+router.get('/add', (req, res) => {
     res.send('deu certo!')
 })
 
 //add job via post
 router.post("/add", (req, res) => {
-  let { title, salalry, company, description, email, new_job } = req.body;
+  let { title, description, company, salary, email, new_job } = req.body;
 
   //insert
-  Job.creat({
+  Job.create({
     title,
-    desciption,
-    salary,
+    description,
     company,
+    salary,
     email,
     new_job,
   })
